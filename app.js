adabbfc7477c10c6ca6a6ac637d93610977c7aaa
samarawwleticia <samaradddsantos@gmail.com>
2025-08-06 13:35:32 -0300

    feat: adicionar array que recebe nomes

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeNomes = [];
+
+
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value; // input recebe o nome
+    listaDeNomes.push(nome);
+    
+}
+
