82d7a36e41dcc72c84883cb27f6d26408fc82512
samarawwleticia <samaradddsantos@gmail.com>
2025-08-06 13:39:42 -0300

    feat: função para agregar amigos + limpar console

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ let listaDeNomes = [];
 
 function adicionarAmigo() {
     let nome = document.querySelector('input').value; // input recebe o nome
-    listaDeNomes.push(nome);
+    if(nome.trim() === ""){ //essa função remove os epsaços em branco no início e no final da string
+        alert('Insira um nome válido!');
+    } else {
+        listaDeNomes.push(nome); // lista de nomes recebe novo nome
+        limparCampo();
+    }
     
 }
 
+function limparCampo() {
+    nome = document.querySelector('input');
+    nome.value = '';
+}
